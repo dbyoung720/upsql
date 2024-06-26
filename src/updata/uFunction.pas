d418e2274401f84af97e0b73a9f66a49d3500dc1
dbyoung <dbyoung@sina.com>
2024-06-26 11:12:41 +0800

    update

--- src/updata/uFunction.pas
+++ src/updata/uFunction.pas
@@ -92,7 +92,7 @@ begin
     strFuncName := frmUpdate.qry1.Fields[0].AsString;
     if not frmUpdate.qry2.Locate('name', strFuncName, []) then
     begin
-      frmUpdate.LogInfo(Format('删除触发器：%s', [strFuncName]));
+      frmUpdate.LogInfo(Format('删除自定义函数：%s', [strFuncName]));
       frmUpdate.qry3.Close;
       frmUpdate.qry3.SQL.Clear;
       frmUpdate.qry3.SQL.Text := Format('drop function %s.dbo.%s', [strOldDataBaseName, strFuncName]);
@@ -101,7 +101,7 @@ begin
       except
         on E: Exception do
         begin
-          frmUpdate.LogInfo(Format('删除触发器失败。触发器名称: %s，原因: %s', [strFuncName, E.Message]));
+          frmUpdate.LogInfo(Format('删除自定义函数失败。触发器名称: %s，原因: %s', [strFuncName, E.Message]));
           frmUpdate.LogInfo(frmUpdate.qry3.SQL.Text);
         end;
       end;
@@ -127,12 +127,6 @@ begin
   frmUpdate.qry4.SQL.Text := Format('exec %s.dbo.sp_helptext %s', [strUpdateDatabaseName, QuotedStr(strFuncName)]);
   frmUpdate.qry4.Open;
 
-  if frmUpdate.qry4.RecordCount <> frmUpdate.qry3.RecordCount then
-  begin
-    Result := False;
-    Exit;
-  end;
-
   Result := True;
   frmUpdate.qry3.First;
   frmUpdate.qry4.First;
@@ -140,10 +134,13 @@ begin
   begin
     strOldFuncCode := Trim(frmUpdate.qry3.Fields[0].AsString);
     strNewFuncCode := Trim(frmUpdate.qry4.Fields[0].AsString);
-    if not SameText(strOldFuncCode, strNewFuncCode) then
+    if (strOldFuncCode <> '') and (strNewFuncCode <> '') then
     begin
-      Result := False;
-      Break;
+			if not SameText(strOldFuncCode, strNewFuncCode) then
+			begin
+				Result := False;
+				Break;
+			end;
     end;
     frmUpdate.qry3.Next;
     frmUpdate.qry4.Next;
@@ -173,7 +170,7 @@ begin
     strFuncName := frmUpdate.qry2.Fields[0].AsString;
     if frmUpdate.qry1.Locate('name', strFuncName, []) then
     begin
-      { 比较两存储过程是否相同 }
+      { 比较两自定义函数是否相同 }
       if not SameFunc(strOldDataBaseName, strUpdateDatabaseName, strFuncName) then
       begin
         frmUpdate.LogInfo(Format('升级自定义函数：%s', [strFuncName]));
